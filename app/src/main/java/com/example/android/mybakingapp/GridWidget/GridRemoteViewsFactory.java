92b11c93b49baa716fae3a623b478f7c88a83365
YokoBurke <fujikurock@gmail.com>
2019-11-17 17:20:36 -0500

    Add Gridview Widgets

---
+++ app/src/main/java/com/example/android/mybakingapp/GridWidget/GridRemoteViewsFactory.java
@@ -0,0 +1,59 @@
+package com.example.android.mybakingapp.GridWidget;
+
+import android.content.Context;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+public class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    Context mContext;
+
+    public GridRemoteViewsFactory (Context applicationContext) {
+        mContext = applicationContext;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        return null;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+}
